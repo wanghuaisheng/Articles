a3b1984a7f79ae47a20285efe4be2f5b5c496e6a
OutMan <hiwhs@qq.com>
2017-10-14 11:18:11 +0800

    Update 还原数据库.sql

--- Tsql/还原数据库.sql
+++ Tsql/还原数据库.sql
@@ -1,16 +1,21 @@
---restore filelistonly from disk='F:\¡Ÿ ±\DotNetSoft_backup_2017_01_03_030001_5519747.bak'
-restore database DotNetSoft_Dcnc from disk = 'F:\¡Ÿ ±\DotNetSoft_backup_2017_01_03_030001_5519747.bak'
-with move 'DotNetSoftTest'     to 'E:\Database\DotNetSoft_Dcnc.mdf'
-    ,move 'DotNetSoftTest_log' to 'E:\Database\DotNetSoft_Dcnc_log.ldf'
+restore filelistonly from disk='D:\Database\TouchMall-2015-12-8.bak'
+restore database TouchMall from disk = 'D:\Database\TouchMall-2015-12-8.bak'
+with move 'TouchMall'     to 'D:\Database\TouchMall.mdf'
+    ,move 'TouchMall_log' to 'D:\Database\TouchMall_log.ldf'
+
+restore filelistonly from disk='D:\Database\Touch10042.bak'
+restore database Touch10042 from disk = 'D:\Database\Touch10042.bak'
+with move 'TouchBackup'     to 'D:\Database\Touch10042.mdf'
+    ,move 'TouchBackup_log' to 'D:\Database\Touch10042_log.ldf'
 
 
-----	restore filelistonly from disk='E:\Database\TouchMall.bak'
-----restore database TouchMall from disk = 'E:\Database\TouchMall.bak'
-----with move 'TouchMall'     to 'E:\Database\TouchMall.mdf'
-----    ,move 'TouchMall_log' to 'E:\Database\TouchMall_log.ldf'
+restore filelistonly from disk='D:\Database\Touch10047.bak'
+restore database Touch10047 from disk = 'D:\Database\Touch10047.bak'
+with move 'TouchBackup'     to 'D:\Database\Touch10047.mdf'
+    ,move 'TouchBackup_log' to 'D:\Database\Touch10047_log.ldf'
 
+restore filelistonly from disk='D:\Database\Touch10054.bak'
+restore database Touch10054 from disk = 'D:\Database\Touch10054.bak'
+with move 'TouchBackup'     to 'D:\Database\Touch10054.mdf'
+    ,move 'TouchBackup_log' to 'D:\Database\Touch10054_log.ldf'
 
-	restore filelistonly from disk='E:\Database\TouchMall3.bak'
-restore database Mall3 from disk = 'E:\Database\TouchMall3.bak'
-with move 'TouchMall3'     to 'E:\Database\Mall3.mdf'
-    ,move 'TouchMall3_log' to 'E:\Database\Mall3_log.ldf'
